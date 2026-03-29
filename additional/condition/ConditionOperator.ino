d65b0d3aceee56d6370555305f544da6e21ba434
vovayush <vova.yush@gmail.com>
2026-03-29 10:26:44 +0300

    condition refactored

--- additional/condition/ConditionOperator.ino
+++ additional/condition/ConditionOperator.ino
@@ -3,22 +3,39 @@ void setup() {
 }
 
 void loop() {
-    int buttonState = 1; // виставляємо вхідне значення
+  int buttonState = 1; // виставляємо вхідне значення
 
-    if (buttonState == HIGH) { // Якщо значення змінної buttonState рівне 1 (5V або HIGH)
-      Serial.println("Pushed"); // то виводимо Pushed
-    } else { // інакше
-      Serial.println("Not pushed");  // інакше Not pushed
-    }
-    delay(1000);
-    int temp = 36;
-    if (temp > 35) {
-      Serial.println("Hot"); // Дуже гаряче!
-    } else if (temp > 20) {
-      Serial.println("Comfortable"); // Комфортно
-    } else {
-      Serial.println("Cold"); // Холодно
-    }
+  if (buttonState == HIGH) { // Якщо значення змінної buttonState рівне 1 (5V або HIGH)
+    Serial.println("Pushed"); // то виводимо Pushed
+  } else { // інакше
+    Serial.println("Not pushed");  // інакше Not pushed
+  }
+  delay(1000);
+  int temp = 36;
+  if (temp > 35) {
+    Serial.println("Hot"); // Дуже гаряче!
+  } else if (temp > 20) {
+    Serial.println("Comfortable"); // Комфортно
+  } else {
+    Serial.println("Cold"); // Холодно
+  }
+  /*
+    == : дорівнює (не плутайте з =, який призначає значення правої частини лівій).
+    != : не дорівнює.
+    >  : лівий операнд більше.
+    <  : лівий операнд менше.
+    >= : більше або дорівнює.
+    <= : менше або дорівнює.
+
+
+    && (І): умова істинна, лише якщо обидві частини правдиві.
+    Наприклад:
+    if (temp > 20 && light == LOW) { ... }
+    || (АБО): умова істинна, якщо хоча б одна частина правдива.
+    if (button1 == HIGH || button2 == HIGH) { ... }
+    ! (НЕ): інвертує значення.
+    if (!isPaused) { ... } (виконувати, якщо НЕ на паузі).
+  */
 
 
   int mode = 1; // Поточний режим
@@ -43,24 +60,29 @@ void loop() {
       break;
   }
 
-    delay(10000);
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+
+    switch (command) {
+      case 'R': // Вмикаємо червоний LED
+        digitalWrite(4, HIGH);
+        break;
+      case 'G': // Вмикаємо зелений LED
+        digitalWrite(5, HIGH);
+        break;
+      case 'S': // Stop (вимикаємо все)
+        digitalWrite(4, LOW);
+        digitalWrite(5, LOW);
+        break;
+    }
+  }
+ /*
+     Декілька умов на одну дію: Ви можете написати декілька case підряд:
+    case 'a':
+    case 'A':
+      // спрацює і для малої, і для великої літери
+      break;
+ */
+  delay(10000);
 }
 
-/*
-    == : дорівнює (не плутайте з =, який призначає значення правої частини лівій).
-    != : не дорівнює.
-    >  : лівий операнд більше.
-    <  : лівий операнд менше.
-    >= : більше або дорівнює.
-    <= : менше або дорівнює.
-
-
-    && (І): умова істинна, лише якщо обидві частини правдиві.
-    Наприклад:
-    if (temp > 20 && light == LOW) { ... }
-    || (АБО): умова істинна, якщо хоча б одна частина правдива.
-    if (button1 == HIGH || button2 == HIGH) { ... }
-    ! (НЕ): інвертує значення.
-    if (!isPaused) { ... } (виконувати, якщо НЕ на паузі).
-
-*/
