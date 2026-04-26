a3576af833389791042ae4ac431b08d553b0d5b9
vovayush <vova.yush@gmail.com>
2026-04-26 16:42:17 +0300

    lighter project refactoring

--- projects/lighter/LighterProject.ino
+++ projects/lighter/LighterProject.ino
@@ -4,42 +4,28 @@ unsigned int  yellowPin = 8;
 unsigned int  greenPin = 4;
 unsigned int  switchPin = 2;
 
-boolean lastButton = LOW;
+// збереження статусу
+boolean lastButton = HIGH;
 boolean lighterOn = false;
-boolean currentButton = LOW;
-
-boolean debounce(boolean last) {
-  boolean current = digitalRead(switchPin);
-  if (last != current) {
-    delay(5);
-    current = digitalRead(switchPin);
-  }
-  return current;
-}
+// HIGH для INPUT_PULLUP режиму
+boolean currentButton = HIGH;
 
 void setup() {
   // налаштування режиму пінів
   pinMode(redPin, OUTPUT);
   pinMode(yellowPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
-  pinMode(switchPin, INPUT);
-
-}
-
-void customDelay(int inputDelay) {
-  int counter = 0;
-  while(counter < inputDelay) {
-    checkButtonStatus();
-    delay(1);
-    counter = counter + 1;
-  }
+  pinMode(switchPin, INPUT_PULLUP);
+  // налаштування дебаг
+  Serial.begin(9600);
 }
 
 void loop() {
   checkButtonStatus();
-
+  // перевіряєм який резим включений, якщо true то працює
   if (lighterOn) {
     turnOnLighter();
+  // інакше моргаючий жовтий
   } else {
     yellowBlinkingLighter();
   }
@@ -48,45 +34,79 @@ void loop() {
 }
 
 void checkButtonStatus(){
+  // вичитуєм значення піна кнопки
   currentButton = debounce(lastButton);
-  // перевірка натиску кнопки
-  if (lastButton == LOW && currentButton == HIGH) {
-    // інвертуємо значення включено на виключено і навпаки
-    lighterOn = !lighterOn;
+  //дебаг для відслідковування натискання, можна відключити
+  if (currentButton == LOW) {
+    Serial.print("currentButton=");
+    Serial.println(currentButton);
+  }
+  // якщо натиснута кнопка
+  if (lastButton == HIGH && currentButton == LOW) {
+    Serial.println("inversion");
+    lighterOn = !lighterOn; // інвертуємо значення
   }
 }
 
 void turnOnLighter() {
-  // блимаємо всіма діодами з певною затримкою
-  blinkLED(redPin, 3000, 200);
-  blinkLED(yellowPin, 1000, 200);
-  blinkLED(greenPin, 3000, 200);
-
-  // моргаючий жовтий перед червоним
-  blinkLED(yellowPin, 1000, 200);
+  // світимо червоне, жовте, зелене, моргаюче жовте перед червоним
+  blinkWithCheck(redPin, 3000, 200);
+  blinkWithCheck(yellowPin, 1000, 200);
+  blinkWithCheck(greenPin, 3000, 200);
+  blinkWithCheck(yellowPin, 1000, 200);
   for(int index = 0; index < 3; index++) {
-    blinkLED(yellowPin, 200, 200);
+    blinkWithCheck(yellowPin, 200, 200);
   }
 }
 
+void blinkWithCheck(int pin, int durationHigh, int durationLow){
+  // якщо світлофор в робочому стані
+  if (lighterOn) {
+    blinkLED(pin, durationHigh, durationLow);
+  }
+}
+
+// усунення тремтіння кнопки
+boolean debounce(boolean last) {
+  boolean current = digitalRead(switchPin);
+  if (last != current) {
+    delay(50);
+    current = digitalRead(switchPin);
+  } else {
+    // додаєм затримку для коректності прорахунку
+    delay(50);
+  }
+  return current;
+}
+
+// гасимо все світло
 void turnOffLighter() {
   digitalWrite(redPin, LOW);
   digitalWrite(yellowPin, LOW);
   digitalWrite(greenPin, LOW);
 }
 
+// режим моргаючого світлофора з відслідковуванням включення
 void yellowBlinkingLighter() {
   turnOffLighter();
   blinkLED(yellowPin, 1000, 500);
 }
 
+// моргання світлодіодом
 void blinkLED(int pin, int durationHigh, int durationLow) {
-  // включення pin
-  digitalWrite(pin, HIGH);
-  // затримка
-  customDelay(durationHigh);
-  // виключення
-  digitalWrite(pin, LOW);
-  // затримка
-  customDelay(durationLow);
+  digitalWrite(pin, HIGH); // включення
+  customDelay(durationHigh); // затримка
+  digitalWrite(pin, LOW); // виключення
+  customDelay(durationLow); // затримка
+}
+
+// налаштування затримки з перевіркою натискання кнопки
+// імітація конкурентності
+void customDelay(int inputDelay) {
+  int counter = 0;
+  while(counter < inputDelay) {
+    checkButtonStatus();
+    delay(5);
+    counter = counter + 55;
+  }
 }
