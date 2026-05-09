93aa79b5425ea0d81699466a6735be6d8e1f7578
vovayush <vova.yush@gmail.com>
2026-05-09 10:22:29 +0300

    thermo resistor refactoring

--- thermo_resistor/ThermoResistorWithLedAndTemperature.ino
+++ thermo_resistor/ThermoResistorWithLedAndTemperature.ino
@@ -1,8 +1,19 @@
+// підключення бібліотеки math
+#include <math.h>
+
 // асоціація змінної з номером піна. Змінна типу int.
 // Пін, до якого підключено дільник
 int sensePin = 0;
 // Пін, до якого підключено світлодіод
 int ledPin = 3;
+
+// Опір постійного резистора (10кОм)
+const float R_FIXED = 10000.0;
+
+// Коефіцієнти Стейнхарта-Харта для MF52A1 (B=3950)
+const float A = 1.106837e-03;
+const float B = 2.384642e-04;
+const float C = 6.507393e-08;
 
 void setup() {
     // налаштування режиму пінів
@@ -14,13 +25,32 @@ void setup() {
 void loop() {
     // вичитуєм значення з аналогового піна
     // до якого підключений фоторезистор
-    int termValue = analogRead(sensePin);
+    int thermValue = analogRead(sensePin);
     Serial.print("Значення з термістора: ");
-    Serial.println(termValue);
+    Serial.println(thermValue);
+
+
+    // 1. Обчислюємо опір термістора
+    // Формула: R = R_fixed * ( (1023 / ADC) - 1 )
+    float resistance = R_FIXED * (1023.0 / (float)thermValue - 1.0);
+    Serial.print("Опір термістора: ");
+    Serial.println(resistance);
+
+    // 2. Формула Стейнхарта-Харта
+    float logR = log(resistance);
+    float tempK = 1.0 / (A + B * logR + C * logR * logR * logR);
+    Serial.print("температура в кельвінах: ");
+    Serial.println(tempK);
+
+    // 3. Переводимо в Цельсії
+    float tempC = tempK - 273.15;
+    Serial.print("температура в цельсіях: ");
+    Serial.println(tempC);
+
 
     // приводимо значення в певний діапазон
     // все що виходить за межі відсікається
-    int value = constrain(termValue, 500, 900);
+    int value = constrain(thermValue, 500, 900);
     Serial.print("стабілізоване значення: ");
     Serial.println(value);
 
